2dac5f4bebbfd48423f8046147882971e384c17d
Mohammad Kermani <mkermani144@gmail.com>
2025-09-04 11:44:00 +0330

    Update error messages

--- src/lingoo/models.rs
+++ src/lingoo/models.rs
@@ -27,12 +27,74 @@ impl LingooChatRequest {
 }
 #[derive(Error, Debug)]
 pub enum LingooChatError {
-  #[error("Error while getting conversation message history")]
+  #[error("error while getting conversation message history")]
   GetConversationMessageHistory(#[from] GetConversationMessageHistoryError),
-  #[error("Error getting a response from LLM")]
+  #[error("error while getting contextual similarity data")]
+  Rag,
+  #[error("error getting a response from LLM")]
   Llm,
-  #[error("Error while storing messages")]
+  #[error("error while storing messages")]
   StoreMessage(#[from] StoreMessageError),
   #[error("unknown error during chat")]
+  Unknown,
+}
+
+#[derive(Debug)]
+pub struct LingooRagDocument {
+  id: Id,
+  embedding: Embedding,
+  content: String,
+  created_at: Timestamp,
+  updated_at: Timestamp,
+}
+impl LingooRagDocument {
+  pub fn new(
+    id: Id,
+    embedding: Embedding,
+    content: String,
+    created_at: Timestamp,
+    updated_at: Timestamp,
+  ) -> Self {
+    Self {
+      id,
+      embedding,
+      content,
+      created_at,
+      updated_at,
+    }
+  }
+
+  pub fn id(&self) -> &Id {
+    &self.id
+  }
+  pub fn embedding(&self) -> &Embedding {
+    &self.embedding
+  }
+  pub fn content(&self) -> &String {
+    &self.content
+  }
+  pub fn created_at(&self) -> &Timestamp {
+    &self.created_at
+  }
+  pub fn updated_at(&self) -> &Timestamp {
+    &self.updated_at
+  }
+}
+
+pub struct FindSimilarDocsRequest {
+  query: Embedding,
+}
+impl FindSimilarDocsRequest {
+  pub fn new(query: Embedding) -> Self {
+    Self { query }
+  }
+  pub fn query(&self) -> &Embedding {
+    &self.query
+  }
+}
+
+#[derive(Error, Debug)]
+pub enum FindSimilarDocsError {
+  #[error("unknown error while finding similar embeddings")]
   Unknown,
 }
