18703fe6c8fc8a83f3881aeef79a4b790d410725
Mohammad Kermani <mkermani144@gmail.com>
2025-10-07 21:26:35 +0330

    Initialize epis_core crate for sharing common types

---
+++ epis-core/src/non_empty_text.rs
@@ -0,0 +1,41 @@
+use derive_more::{AsRef, Into};
+use thiserror::Error;
+
+#[derive(Debug, Clone, AsRef, PartialEq, Eq, PartialOrd, Ord, Hash, Into)]
+#[as_ref(forward)]
+pub struct NonEmptyString(String);
+
+#[derive(Debug, Clone, Error)]
+pub enum NonEmptyStringError {
+  #[error("provided string is empty")]
+  Empty,
+}
+
+impl NonEmptyString {
+  pub fn new(value: String) -> Result<Self, NonEmptyStringError> {
+    if value.is_empty() {
+      return Err(NonEmptyStringError::Empty);
+    }
+    Ok(Self(value))
+  }
+}
+
+impl TryFrom<String> for NonEmptyString {
+  type Error = NonEmptyStringError;
+  fn try_from(value: String) -> Result<Self, Self::Error> {
+    Self::new(value)
+  }
+}
+
+impl TryFrom<&str> for NonEmptyString {
+  type Error = NonEmptyStringError;
+  fn try_from(value: &str) -> Result<Self, Self::Error> {
+    Self::new(value.to_owned())
+  }
+}
+
+impl From<NonEmptyString> for &str {
+  fn from(value: NonEmptyString) -> Self {
+    value.into()
+  }
+}
