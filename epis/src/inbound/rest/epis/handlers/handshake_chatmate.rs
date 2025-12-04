aa92a5b604a2b78e94469a0bc396e54350e82863
Mohammad Kermani <mkermani144@gmail.com>
2025-12-04 19:15:46 +0330

    Return chatmate id in handshake API

--- epis/src/inbound/rest/epis/handlers/handshake_chatmate.rs
+++ epis/src/inbound/rest/epis/handlers/handshake_chatmate.rs
@@ -3,15 +3,15 @@
 use std::str::FromStr;
 
 use axum::{Extension, Json, extract::State, http::StatusCode, response::IntoResponse};
-use clerk_rs::validators::authorizer::ClerkJwt;
 use derive_getters::Getters;
+use derive_more::Constructor;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use utoipa::ToSchema;
 
 use crate::{
   domain::{
-    models::{ChatMateLanguage, EpisError},
+    models::{ChatMateLanguage, EpisError, User},
     ports::{Epis, UserManagement},
   },
   inbound::{http::AppState, rest::epis::EPIS_CATEGORY},
@@ -47,6 +47,13 @@ pub struct HandshakeChatmateRequestBody {
   language: String,
 }
 
+/// Body of the response
+#[derive(Debug, Clone, Constructor, Serialize, ToSchema)]
+pub struct HandshakeChatmateResponse {
+  /// Id of the created chatmate
+  chatmate_id: String,
+}
+
 /// Handshake chatmate handler
 #[utoipa::path(
   post,
@@ -54,21 +61,20 @@ pub struct HandshakeChatmateRequestBody {
   tag = EPIS_CATEGORY,
   request_body = HandshakeChatmateRequestBody,
   responses(
-    (status = CREATED, content_type = "application/json"),
+    (status = CREATED, body = HandshakeChatmateResponse, content_type = "application/json"),
     (status = BAD_REQUEST, body = String, content_type = "application/json"),
     (status = INTERNAL_SERVER_ERROR, body = String, content_type = "application/json"),
   )
 )]
 pub async fn handshake_chatmate<E: Epis, UM: UserManagement>(
   State(app_state): State<AppState<E, UM>>,
-  Extension(jwt): Extension<ClerkJwt>,
+  Extension(user): Extension<User>,
   Json(request): Json<HandshakeChatmateRequestBody>,
-) -> Result<Json<()>, HandshakeChatmateApiError> {
-  let user_id = jwt.sub;
-  app_state
+) -> Result<Json<HandshakeChatmateResponse>, HandshakeChatmateApiError> {
+  let chatmate = app_state
     .epis()
     .handshake(
-      &user_id,
+      user.id(),
       &ChatMateLanguage::from_str(&request.language)
         .map_err(|_| HandshakeChatmateApiError::UnsupportedLanguage)?,
     )
@@ -78,5 +84,7 @@ pub async fn handshake_chatmate<E: Epis, UM: UserManagement>(
       _ => HandshakeChatmateApiError::Unknown,
     })?;
 
-  Ok(Json(()))
+  Ok(Json(HandshakeChatmateResponse::new(
+    chatmate.id().to_string(),
+  )))
 }
